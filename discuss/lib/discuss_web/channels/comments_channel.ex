3f014d2f15ae1eeda1c22db30ef111cbc22e9afc
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2019-10-29 14:24:36 -0300

    add comment feature

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,13 +1,41 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
 
-  def join(name, _params, socket) do
-    IO.puts("++++++")
-    IO.puts(name)
-    {:ok, %{}, socket}
+  alias Discuss.Model.{Topic, Comment, User}
+  alias Discuss.Repo
+
+  def join("comments:" <> topicId, _params, socket) do
+    topic_id = String.to_integer(topicId);
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, message, socket) do
+    %{"content" => content} = message
+    topic = socket.assigns.topic
+    user = Repo.get(User, socket.assigns.user_id)
+
+    changeset = topic
+      |> Ecto.build_assoc(:comments)
+      |> Ecto.Changeset.change()
+      |> Ecto.Changeset.put_assoc(:user, user)
+      |> Comment.changeset(%{content: content})
+
+    IO.inspect(changeset)
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+          %{comment: comment}
+        )
+
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
 
   end
 
--- discuss/lib/discuss_web/channels/user_socket.ex
+++ discuss/lib/discuss_web/channels/user_socket.ex
@@ -3,7 +3,7 @@ defmodule DiscussWeb.UserSocket do
 
   ## Channels
   channel "comments:*", DiscussWeb.CommentsChannel
-  
+
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
@@ -15,8 +15,13 @@ defmodule DiscussWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
